6e154d500037310fd7aba359b20f35d943b44390
Troy Jacobson <troy@thismuch.net>
2021-08-05 03:16:38 +0100

    Cleanup

--- src/components/widgets/gcode-preview/GcodePreview.vue
+++ src/components/widgets/gcode-preview/GcodePreview.vue
@@ -287,9 +287,7 @@ export default class GcodePreview extends Mixins(StateMixin) {
   }
 
   get svgParts () {
-    console.log('In svgParts')
     const svg = this.$store.getters['gcodePreview/getPartsSVG']
-    console.log(svg)
     return svg
   }
 
@@ -305,8 +303,6 @@ export default class GcodePreview extends Mixins(StateMixin) {
   }
 
   onPartClick (id: string, e?: any) {
-    console.log('Clicked on ' + id)
-    console.log(e)
     // Let's pick a random color between #000000 and #FFFFFF
     const color = Math.round(Math.random() * 0xFFFFFF)
 
